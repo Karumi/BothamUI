cac1df48fb4bf34c96de7af491b1d1c1550a01ce
Davide Mendolia <davide@gokarumi.com>
2015-10-25 14:26:22 +0100

    Initial Commit

---
+++ BothamUI/BothamUI.h
@@ -0,0 +1,19 @@
+//
+//  BothamUI.h
+//  BothamUI
+//
+//  Created by Davide Mendolia on 25/10/15.
+//  Copyright © 2015 GoKarumi S.L. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BothamUI.
+FOUNDATION_EXPORT double BothamUIVersionNumber;
+
+//! Project version string for BothamUI.
+FOUNDATION_EXPORT const unsigned char BothamUIVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BothamUI/PublicHeader.h>
+
+
