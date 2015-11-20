306f2a1496dadf346bb519b8a10965b790c59c86
Davide Mendolia <davide@gokarumi.com>
2015-11-20 13:54:16 +0100

    Fix merge issue

--- BothamUI/BothamTableViewDataSource.swift
+++ BothamUI/BothamTableViewDataSource.swift
@@ -8,12 +8,8 @@
 
 import Foundation
 
-<<<<<<< HEAD
-public class BothamTableViewDataSource<U, V: BothamTableViewCell where U == V.ItemType>
+public class BothamTableViewDataSource<U, V: BothamViewCell where U == V.ItemType>
                                       : NSObject, UITableViewDataSource {
-=======
-public class BothamTableViewDataSource<U, V: BothamViewCell where U == V.ItemType>: NSObject, UITableViewDataSource {
->>>>>>> 5346ee4... Add support of CollectionView
     public var items: [U] = []
 
     public override init() {
