aa210ff37e5ced41c2df0820e67e9b9e128affe3
Davide Mendolia <davide@gokarumi.com>
2015-11-20 15:45:10 +0100

    Removed useless coment

--- Example/Example/ComicsPresenter.swift
+++ Example/Example/ComicsPresenter.swift
@@ -17,11 +17,6 @@ class ComicsPresenter: BothamPresenter {
     }
 
     func viewDidLoad() {
-//        http://i.annihil.us/u/prod/marvel/i/mg/9/40/50b4fc783d30f.jpg
-//        http://i.annihil.us/u/prod/marvel/i/mg/c/30/4fe8cb51f32e0.jpg
-//        http://i.annihil.us/u/prod/marvel/i/mg/6/03/502912f2a8809.jpg
-//        http://i.annihil.us/u/prod/marvel/i/mg/e/b0/564a1abbcf9b0.jpg
-
         self.ui?.showItems([Comic(cover: UIImage(named: "50b4fc783d30f")),
                             Comic(cover: UIImage(named: "4fe8cb51f32e0")),
                             Comic(cover: UIImage(named: "502912f2a8809")),
