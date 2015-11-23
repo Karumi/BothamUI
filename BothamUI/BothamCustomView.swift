999c3548ed82605693820f84b964a85ea6585c11
Davide Mendolia <davide@gokarumi.com>
2015-11-23 15:53:20 +0100

    Add custom Loading View

---
+++ BothamUI/BothamCustomView.swift
@@ -0,0 +1,34 @@
+//
+//  BothamCustomView.swift
+//  BothamUI
+//
+//  Created by Davide Mendolia on 23/11/15.
+//  Copyright © 2015 GoKarumi S.L. All rights reserved.
+//
+
+import Foundation
+
+public class BothamCustomView: UIView {
+    var view: UIView!
+
+    public override init(frame: CGRect) {
+        super.init(frame: frame)
+        xibSetup()
+    }
+
+    public required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        xibSetup()
+    }
+
+    func xibSetup() {
+        view = loadViewFromNib()
+        view.frame = bounds
+        view.autoresizingMask = [UIViewAutoresizing.FlexibleWidth, UIViewAutoresizing.FlexibleHeight]
+        addSubview(view)
+    }
+
+    func loadViewFromNib() -> UIView {
+        return NSBundle(forClass: self.dynamicType).loadNibNamed("LoadingView", owner: self, options: nil) [0] as! UIView
+    }
+}
