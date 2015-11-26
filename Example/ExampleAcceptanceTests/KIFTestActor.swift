0d31f9c8d2bacf6af622878f5b3a4b14d50f1772
Pedro Vicente Gomez <pedro@gokarumi.com>
2015-11-26 15:52:07 +0100

    Configure BridgingHeader file and the needed extensions to be able to use KIF from swift

---
+++ Example/ExampleAcceptanceTests/KIFTestActor.swift
@@ -0,0 +1,20 @@
+//
+//  KIFTestActor.swift
+//  Example
+//
+//  Created by Pedro Vicente Gomez on 26/11/15.
+//  Copyright © 2015 GoKarumi S.L. All rights reserved.
+//
+
+import Foundation
+import KIF
+
+extension KIFTestActor {
+    func tester(file : String = __FILE__, line : Int = __LINE__) -> KIFUITestActor {
+        return KIFUITestActor(inFile: file, atLine: line, delegate: self)
+    }
+
+    func system(file : String = __FILE__, line : Int = __LINE__) -> KIFSystemTestActor {
+        return KIFSystemTestActor(inFile: file, atLine: line, delegate: self)
+    }
+}
