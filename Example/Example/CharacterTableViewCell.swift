c6c21ccbe0fc9c4bf7a76c3d0cab4ed54adb45cd
Sergio Gutierrez <sergio@gokarumi.com>
2015-12-18 12:45:42 +0100

    Configure characters view cells

--- Example/Example/CharacterTableViewCell.swift
+++ Example/Example/CharacterTableViewCell.swift
@@ -8,10 +8,23 @@
 
 import Foundation
 import BothamUI
+import SDWebImage
 
 class CharacterTableViewCell: UITableViewCell, BothamViewCell {
+
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var thumbnailImage: UIImageView!
+
     func configureForItem(item: Character) {
-        self.textLabel?.text = item.name
-        self.textLabel?.accessibilityLabel = item.name
+        nameLabel.text = item.name
+        thumbnailImage.sd_setImageWithURL(item.thumbnail)
+
+        let gradient: CAGradientLayer = CAGradientLayer(layer: thumbnailImage.layer)
+        gradient.frame = thumbnailImage.bounds
+        gradient.colors = [UIColor(red: 44.0 / 255.0, green: 52.0 / 255.0, blue: 60.0 / 255.0, alpha: 0.0).CGColor, UIColor(red: 44.0 / 255.0, green: 52.0 / 255.0, blue: 60.0 / 255.0, alpha: 0.9).CGColor]
+        gradient.startPoint = CGPointMake(0.0, 0.6)
+        gradient.endPoint = CGPointMake(0.0, 1.0)
+
+        thumbnailImage.layer.insertSublayer(gradient, atIndex: 0)
     }
 }
