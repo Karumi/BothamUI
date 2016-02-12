efee38f3e31f51e84d6e23411da0accc3e228a78
Davide Mendolia <davide@gokarumi.com>
2016-02-12 13:04:59 +0100

    Add Cocoa Example

---
+++ ExampleCocoa/ExampleCocoa/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  ExampleCocoa
+//
+//  Created by Davide Mendolia on 12/02/16.
+//  Copyright © 2016 Karumi. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: AnyObject? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
