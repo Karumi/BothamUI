4b32bc90fefad06288685e0252b647e87987039a
Davide Mendolia <davide@gokarumi.com>
2016-04-05 19:48:44 +0200

    Fix initialiser visibility

--- Sources/BothamViewController.swift
+++ Sources/BothamViewController.swift
@@ -13,11 +13,11 @@ import Foundation
 public class BothamViewController<Presenter: BothamPresenter>: UIViewController, BothamUI {
     public var presenter: Presenter! = nil
 
-    public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+    @objc public override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
         super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
     }
 
-    public required init?(coder aDecoder: NSCoder) {
+    @objc public required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
 
