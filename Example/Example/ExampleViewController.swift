48edd5ceb80638ab0fbc29709bd2ce11b06a80fc
Davide Mendolia <davide@gokarumi.com>
2016-04-06 11:11:12 +0200

    Add required initialiser

--- Example/Example/ExampleViewController.swift
+++ Example/Example/ExampleViewController.swift
@@ -11,6 +11,14 @@ import BothamUI
 
 class ExampleViewController<Presenter: BothamPresenter>: BothamViewController<Presenter>, BothamLoadingViewController, BothamPullToRefresh {
 
+    @objc override init(nibName nibNameOrNil: String?, bundle nibBundleOrNil: NSBundle?) {
+        super.init(nibName: nibNameOrNil, bundle: nibBundleOrNil)
+    }
+
+    @objc required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+    }
+
     let loadingView: UIView = {
         let loadingView = BothamLoadingView()
         loadingView.color = UIColor.loadingColor
