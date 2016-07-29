8011aeb2c6952fb77b2480f0b57bffbdc8c55186
Davide Mendolia <davide@gokarumi.com>
2016-07-29 22:57:42 +0200

    Add support for 2.3

--- Sources/NSBundle.swift
+++ Sources/NSBundle.swift
@@ -12,7 +12,11 @@ extension NSBundle {
 #if os(iOS) || os(watchOS) || os(tvOS)
 
     public func loadNib<T: UIView>(name name: String, owner: AnyObject? = nil) -> T {
-        return self.loadNibNamed(name, owner: owner, options: nil)[0] as! T
+#if swift(>=2.3)
+    return self.loadNibNamed(name, owner: owner, options: nil)![0] as! T
+#else
+    return self.loadNibNamed(name, owner: owner, options: nil)[0] as! T
+#endif
     }
 #elseif os(OSX)
     public func loadNib<T: NSView>(name name: String, owner: AnyObject? = nil) -> T {
